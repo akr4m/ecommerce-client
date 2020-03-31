f3266c2a691619d5b2b4b2a702d806f7b98aa7a6
gabbar <akr4m.kh@gmail.com>
2020-03-31 06:44:49 +0600

    Category done

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,6 +49,7 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
+    baseURL: 'http://127.0.0.1:8000/api'
   },
   /*
   ** Build configuration
---
+++ pages/categories/_slug.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        Products for category
+    </div>
+</template>
